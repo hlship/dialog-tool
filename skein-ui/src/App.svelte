aae105235d7ba928150a770b0a570b1d7902172a
Howard Lewis Ship <hlship@gmail.com>
2024-07-28 19:44:09 -0700

    Add TailwindCSS

--- skein-ui/src/App.svelte
+++ skein-ui/src/App.svelte
@@ -11,14 +11,21 @@
   load_nodes()
 
 </script>
+<div class="container">
 
-<h1>Dialog Skein</h1>
+<h1>Dialog Skei?n</h1>
+<h1 class="text-3xl font-bold underline">
+  Hello world!
+</h1>
+
 
 <ul>
 {#each nodes as node (node.id)}
   <li><SkNode {node}/></li>
 {/each}
 </ul>
+
+</div>
 
 
 
