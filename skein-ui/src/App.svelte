13f3ce69d9eb3d807fbf73061f1bb95d1828b96c
Howard Lewis Ship <hlship@gmail.com>
2024-08-01 21:27:57 -0700

    Checkpoint

--- skein-ui/src/App.svelte
+++ skein-ui/src/App.svelte
@@ -1,31 +1,28 @@
 <script>
-  import SkNode from './lib/SkNode.svelte'
+  import SkNode from "./lib/SkNode.svelte";
+  import { onMount } from "svelte";
 
-  let nodes = [{"id": 1, "command": "get help"}]
+  let nodes;
 
-  async function load_nodes() {
-    let response = await fetch("//localhost:10140/api")
-    nodes = await response.json()
-  }
-
-  load_nodes()
-
+  onMount(async () => {
+    let response = await fetch("//localhost:10140/api");
+    nodes = await response.json();
+  });
 </script>
-<div class="container">
 
-<h1>Dialog Skei?n</h1>
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<div class="container mx-lg mx-auto px-8 py-4 d">
+  <h1 class="text-emerald-600 text-3xl">Dialog Skein</h1>
 
-
-<ul>
-{#each nodes as node (node.id)}
-  <li><SkNode {node}/></li>
-{/each}
-</ul>
-
+  <p>This is a whole lot of text about the Skein, and how you use it, and how to navigate
+    and how you use it for testing, and how it's neat as sin, and a great idea that
+    I can't take credit for.
+  </p>
+ 
+  {#if nodes}
+  <ul>
+    {#each nodes as node (node.id)}
+      <li><SkNode {node} /></li>
+    {/each}
+  </ul>
+  {/if}
 </div>
-
-
-
