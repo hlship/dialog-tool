14abf1d32393ac6284116e24883ab3a511bf44bd
Howard M. Lewis Ship <hlship@gmail.com>
2025-12-02 18:17:32 -0800

    More minor progress

--- src/dialog_tool/skein/routes/app.clj
+++ src/dialog_tool/skein/routes/app.clj
@@ -1,5 +1,6 @@
 (ns dialog-tool.skein.routes.app
   (:require [clj-simple-router.core :as router]
+            [clojure.string :as string]
             [dialog-tool.skein.tree :as tree]
             [huff2.core :refer [html]]))
 
@@ -21,15 +22,25 @@
      :d         "M8.6 5.2A1 1 0 0 0 7 6v12a1 1 0 0 0 1.6.8l8-6a1 1 0 0 0 0-1.6l-8-6Z"
      :clip-rule "evenodd"}]])
 
-(defn nav-button [body]
-  [:button {:type  "button"
-            :class "text-center font-medium 
-            focus-within:ring-4 focus-within:outline-none 
-            inline-flex items-center
-            justify-center px-3 py-2 text-xs text-white
-            bg-blue-700
-            hover:bg-blue-800
-            rounded-lg ml-8"}
+(defn- trim
+  [& s]
+  (-> (string/join " " s)
+      (string/replace #"\s+" " ")))
+
+(def button-base
+  (trim "text-center font-medium"
+        "focus-within:ring-4 focus-within:outline-none"
+        "inline-flex items-center"
+        "justify-center px-3 py-2 text-xs text-white"
+        "rounded-lg ml-8"))
+
+(def blue-button
+  (str button-base " bg-blue-700 hover:bg-blue-800"))
+
+(defn nav-button [attrs body]
+  [:button (merge {:type  "button"
+                   :class blue-button}
+                  attrs)
    body])
 
 (defn icon-floppy-disk
@@ -40,13 +51,15 @@
            :clip-rule "evenodd"}]
    [:path {:fill-rule "evenodd"
            :d         "M14 17h-4v-2h4v2Z"
-           :clip-rule "even-odd"}]])
+           :clip-rule "evenodd"}]])
 
 (defn navbar
   [title tree]
   (let [{:keys [new error ok]} (tree/counts tree)]
-    [:nav {:class "bg-white text-gray-500 border-gray-200 divide-gray-200
-  px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 start-0 border-b"}
+    [:nav {:class        (trim "bg-white text-gray-500 border-gray-200 divide-gray-200"
+                               "px-2 sm:px-4 py-2.5"
+                               "fixed w-full z-20 top-0 start-0 border-b")
+           :data-signals "{dirty: false}"}
      [:div.mx-auto.flex.flex-wrap.justify-between.items-center.container
       [:a.flex.items-center
        [:div.self-center.whitespace-nowrap.text-xl.font-semibold
@@ -55,17 +68,19 @@
        [:div.text-black.bg-green-400.p-2.font-semibold.rounded-l-lg ok]
        [:div.text-black.bg-yellow-200.p-2.font-semibold new]
        [:div.text-black.bg-red-500.p-2.font-semibold.rounded-r-lg error]
-       [nav-button "Jump"]
+       [nav-button nil "Jump"]
        [:div.flex.md:order-2.space-x-2
-        [nav-button [:<> [icon-play] "Replay All"]]
-        [nav-button [:<> [icon-floppy-disk] "Save"]]
-        [nav-button "Undo"]
-        [nav-button "Redo"]
-        [nav-button "Quit"]]]]]))
+        [nav-button {:data-on:click "@get('/action/replay-all')"} [:<> [icon-play] "Replay All"]]
+        [nav-button {:class      button-base
+                     :data-class "{'bg-blue-700': $dirty, 'hover:bg-blue-800': $dirty, 'bg-green-700': !$dirty, 'hover:bg-green-800': !$dirty}"}
+         [:<> [icon-floppy-disk] "Save"]]
+        [nav-button nil "Undo"]
+        [nav-button nil "Redo"]
+        [nav-button nil "Quit"]]]]]))
 
 (defn render-knot
-  [{:keys [id response] :as knot}]
-  [:div.border-x-4.border-slate-100 {:id (str "knot-" id)}  ;; TODO: Color
+  [{:keys [id response]}]
+  [:div.border-x-4.border-slate-100 {:id id}                ;; TODO: Color by category
    [:div.bg-yellow-50.w-full.whitespace-pre.relative.p-2
     ;; TODO: All the controls                                                      
     ;; TODO: Show the diff when unblessed not nil
