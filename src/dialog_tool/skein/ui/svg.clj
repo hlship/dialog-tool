c7b6ebd554fb80db4f56d33892cc871776ebea11
Howard M. Lewis Ship <hlship@gmail.com>
2025-12-19 16:01:01 -0800

    Remove dead code

--- src/dialog_tool/skein/ui/svg.clj
+++ src/dialog_tool/skein/ui/svg.clj
@@ -2,14 +2,6 @@
   (:require [clojure.java.io :as io]
             [huff2.core :as h]))
 
-(defn svg [label & body]
-  [:svg {:xmlns      "https://www.w3.org/2000/svg"
-         :fill       "currentColor"
-         :class      "shrink-0 w-5 h-5 me-2"
-         :aria-label label
-         :view-box   "0 0 24 24"}
-   body])
-
 (defn- read-svg*
   [name]
   (let [file (str "icons/" name ".svg")]
